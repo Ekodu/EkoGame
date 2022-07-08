1e48d62293ef1e2a4149e805d7309c13a1a98957
Ekodu <106890143+Ekodu@users.noreply.github.com>
2022-07-08 15:53:00 +0400

    Update botTesla.js

--- botTesla.js
+++ botTesla.js
@@ -428,7 +428,7 @@ bot.start(async (ctx) => {
         reply_markup:{
             inline_keyboard:[
                 [{text:'Botu Qrupa Əlavə Edin ✅', url:`https://t.me/${process.env.BOT_ISMI}?startgroup=true`}],
-                [{text:'Rəsmi Kanalımız 📣', url:`t.me/Rahid_44`},{text:'Qruplar 💎', callback_data:'vip'}]
+                [{text:'Rəsmi Kanalımız 📣', url:`t.me/EkoBlogs`},{text:'Qruplar 💎', callback_data:'vip'}]
             ]
         }
     })
@@ -441,7 +441,7 @@ bot.action('start', ctx=>{
         reply_markup:{
             inline_keyboard:[
                 [{text:'Botu Qrupa Əlavə Edin ✅', url:`t.me/${process.env.BOT_ISMI}?startgroup=true`}],
-                [{text:'Rəsmi Kanalımız 📣', url:`t.me/Rahid_44`},{text:'Qruplar 💎', callback_data:'vip'}]
+                [{text:'Rəsmi Kanalımız 📣', url:`t.me/EkoBlogs`},{text:'Qruplar 💎', callback_data:'vip'}]
             ]
         }
     })
@@ -468,8 +468,7 @@ bot.action('AZ', ctx=>{
     ctx.replyWithMarkdown(`*🇦🇿 Qruplar*`,{
         reply_markup:{
             inline_keyboard:[
-                [{text:'1) Qrup ', url:'t.me/Gencler_Mekani'}],
-                [{text:'2) Qrup ', url:'t.me/GG_teams'}],
+                [{text:'1) Qrup ', url:'t.me/BakuCity_Az'}],
                 [{text:'🔙 Geri', callback_data:'vip'}]
             ]
         }
@@ -483,9 +482,8 @@ bot.action('TR', ctx=>{
 *🇦🇿 Digər Qruplar*
        `,{
         reply_markup:{
-            inline_keyboard:[
-                [{text:'1) Qrup', url:'t.me/BanditosQrup'}],
-                [{text:'2) Qrup', url:'t.me/gecepayizi'}],
+            inline_keyboard:
+               
                 [{text:'🔙 Geri', callback_data:'vip'}]
             ]
         }
